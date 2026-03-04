85fa188856e32f06cfa7ebc138b8f4065d333c04
VikasNaik22 <vikasnaik22102005@gmail.com>
2026-03-04 19:10:33 +0530

    Added module4 and module5 folders

---
+++ module5/ExceptionDemo.java
@@ -0,0 +1,18 @@
+package Module5;
+
+import java.io.*;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Unchecked Exception: " + e);
+        }
+        try {
+            FileReader fr = new FileReader("file.txt");
+        } catch (IOException e) {
+            System.out.println("Checked Exception: " + e);
+        }
+    }
+}
